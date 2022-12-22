105e77f06e274694800b90f16c73d5390733e8b4
Mahmoud Nabeel <mnitpro@gmail.com>
2022-12-23 00:36:10 +0300

    cas: SHRK-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_cas.mk
+    $(LOCAL_DIR)/twrp_cas.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_cas-user \
-    omni_cas-userdebug \
-    omni_cas-eng
+    twrp_cas-user \
+    twrp_cas-userdebug \
+    twrp_cas-eng
